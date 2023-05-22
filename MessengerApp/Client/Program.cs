8812d8577fa8634d55764a7d2a4ead5ca671b7c4
maybeeK <rom.bosyj@gmail.com>
2023-05-22 16:41:26 +0300

    added services

--- MessengerApp/Client/Program.cs
+++ MessengerApp/Client/Program.cs
@@ -1,4 +1,7 @@
 using MessengerApp.Client;
+using MessengerApp.Client.Sevices;
+using MessengerApp.Client.Sevices.Interfaces;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -18,6 +21,9 @@ namespace MessengerApp.Client
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MessengerApp.ServerAPI"));
+
+            builder.Services.AddScoped<IUserDirectService, UserDirectService>();
+            builder.Services.AddScoped<HttpClient>(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7235/") });
 
             builder.Services.AddApiAuthorization();
 
