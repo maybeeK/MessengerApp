b352315cc5aa1431b969efa245bfd75a981b0e06
maybeeK <rom.bosyj@gmail.com>
2023-05-22 13:15:51 +0300

    Start work with client side

--- MessengerApp/Client/Pages/DirectBase.cs
+++ MessengerApp/Client/Pages/DirectBase.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using MessengerApp.Client.Sevices.Interfaces;
+using MessengerApp.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace MessengerApp.Client.Pages
 {
     public class DirectBase: ComponentBase
     {
+        [Inject]
+        public IUserDirectService UserDirectService { get; set; }
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+        public string? UserId { get; set; }
+        public List<ChatDTO>? UserChats { get; set;}
+        public List<MessageDTO>? ChatMessages { get; set;}
 
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                var user = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User.Identity?.Name;
+
+                UserChats = (await UserDirectService.GetChats(UserId)).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
