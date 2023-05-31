cc139459cffe605cd439d073424da4c6e218d1d3
maybeeK <rom.bosyj@gmail.com>
2023-05-31 12:58:27 +0300

    Created entities for online users

---
+++ MessengerApp/Server/Entyties/OnlineUser.cs
@@ -0,0 +1,19 @@
+﻿using MessengerApp.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Server.Entyties
+{
+    public class OnlineUser
+    {
+        public string OnlineUserConnection { get; set; }
+        public string OnlineUserId { get; set; }
+
+        [ForeignKey("OnlineUserId")]
+        public virtual ApplicationUser User { get; set; }
+    }
+}
---
+++ MessengerApp/Shared/DTOs/OnlineUserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Shared.DTOs
+{
+    public class OnlineUserDTO
+    {
+        public string OnlineUserConnectionId { get; set; }
+        public string OnlineUserId { get; set; }
+    }
+}
