30331ca51e0f42d18f1c6738f0379acaf3f59427
maybeeK <rom.bosyj@gmail.com>
2023-06-10 17:38:09 +0300

    Added service for messages encryption

---
+++ MessengerApp/Server/Services/EncryptionService.cs
@@ -0,0 +1,51 @@
+﻿using MessengerApp.Client.Sevices.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MessengerApp.Client.Sevices
+{
+    public class EncryptionService : IEncryptionService
+    {
+        private byte[] _initializationVector =
+        {
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+            0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+        };
+        public async Task<string> EncryptAsync(string textToEncrypt, string passphrase)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = DeriveKeyFromPassword(passphrase);
+            aes.IV = _initializationVector;
+            using MemoryStream output = new();
+            using CryptoStream cryptoStream = new(output, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            await cryptoStream.WriteAsync(Encoding.Unicode.GetBytes(textToEncrypt));
+            await cryptoStream.FlushFinalBlockAsync();
+            string encrypted = String.Join("", output.ToArray());
+            return encrypted;
+        }
+        public async Task<string> DecryptAsync(string encryptedText, string passphrase)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = DeriveKeyFromPassword(passphrase);
+            aes.IV = _initializationVector;
+            byte[] inputBytes = encryptedText.Select(Convert.ToByte).ToArray();
+            using MemoryStream input = new(inputBytes);
+            using CryptoStream cryptoStream = new(input, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using MemoryStream output = new();
+            await cryptoStream.CopyToAsync(output);
+            return Encoding.Unicode.GetString(output.ToArray());
+        }
+        private byte[] DeriveKeyFromPassword(string password)
+        {
+            var emptySalt = Array.Empty<byte>();
+            var iterations = 1000;
+            var desiredKeyLength = 16;
+            var hashMethod = HashAlgorithmName.SHA384;
+            return Rfc2898DeriveBytes.Pbkdf2(Encoding.Unicode.GetBytes(password),
+                                             emptySalt,
+                                             iterations,
+                                             hashMethod,
+                                             desiredKeyLength);
+        }
+    }
+}
---
+++ MessengerApp/Server/Services/Interfaces/IEncryptionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerApp.Client.Sevices.Interfaces
+{
+    public interface IEncryptionService
+    {
+        Task<string> EncryptAsync(string clearText, string passphrase);
+        Task<string> DecryptAsync(string encrypted, string passphrase);
+    }
+}
