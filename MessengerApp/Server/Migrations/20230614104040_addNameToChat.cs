00bce11bbd5036125e8e262492aad87ba7b8e19c
maybeeK <rom.bosyj@gmail.com>
2023-06-14 15:44:40 +0300

    Migration

---
+++ MessengerApp/Server/Migrations/20230614104040_addNameToChat.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MessengerApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNameToChat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Chats",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Chats");
+        }
+    }
+}
